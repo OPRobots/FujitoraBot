b6fcfe7db52df1a33ebaaf4ae63075f5a4d415c5
Alex Santos <alex.santos.ete@gmail.com>
2019-08-09 15:01:10 +0200

    Añade configuración de TIM8 para PWM ESC

--- src/src/main.c
+++ src/src/main.c
@@ -14,9 +14,29 @@ void sys_tick_handler(void) {
 int main(void) {
 
   setup();
-  calibrate_sensors();
+  // calibrate_sensors();
+
+  timer_enable_oc_output(TIM8, TIM_OC1);
+  timer_enable_oc_output(TIM8, TIM_OC2);
+  timer_enable_oc_output(TIM8, TIM_OC3);
+  timer_enable_oc_output(TIM8, TIM_OC4);
+  // timer_set_oc_value(TIM8, TIM_OC1, 1024);
+  // timer_set_oc_value(TIM8, TIM_OC2, 1024);
+  // timer_set_oc_value(TIM8, TIM_OC3, 1024);
+  // timer_set_oc_value(TIM8, TIM_OC4, 1024);
+  // delay(5000);
+  timer_set_oc_value(TIM8, TIM_OC1, 768);
+  timer_set_oc_value(TIM8, TIM_OC2, 768);
+  timer_set_oc_value(TIM8, TIM_OC3, 768);
+  timer_set_oc_value(TIM8, TIM_OC4, 768);
+  delay(5000);
 
   while (1) {
-    debug_line_position();
+    // debug_line_position();
+    
+      // timer_set_oc_value(TIM8, TIM_OC1, 850);
+      // timer_set_oc_value(TIM8, TIM_OC2, 850); // derecho
+      // timer_set_oc_value(TIM8, TIM_OC3, 850); 
+      // timer_set_oc_value(TIM8, TIM_OC4, 850); // izquierdo
   }
 }
--- src/src/setup.c
+++ src/src/setup.c
@@ -24,7 +24,7 @@ static void setup_clock() {
 
   rcc_periph_clock_enable(RCC_TIM1);
   //   rcc_periph_clock_enable(RCC_TIM2);
-  //   rcc_periph_clock_enable(RCC_TIM8);
+    rcc_periph_clock_enable(RCC_TIM8);
 
   rcc_periph_clock_enable(RCC_ADC1);
 
@@ -89,7 +89,8 @@ static void setup_gpio() {
   gpio_set_af(GPIOA, GPIO_AF1, GPIO8 | GPIO9 | GPIO10 | GPIO11);
 
   // Salida PWM Motores
-  gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO6 | GPIO7 | GPIO8 | GPIO9);
+  gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7 | GPIO8 | GPIO9);
+  gpio_set_af(GPIOC, GPIO_AF3, GPIO6 | GPIO7 | GPIO8 | GPIO9);
 
   // USART3
   gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10 | GPIO11);
@@ -176,12 +177,35 @@ static void setup_leds_pwm(void) {
   timer_enable_counter(TIM1);
 }
 
+
+static void setup_motors_pwm(void) {
+  timer_set_mode(TIM8, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+//84000000
+  timer_set_prescaler(TIM8, rcc_apb2_frequency * 2 / 4000000 -2 );
+  // 400000 es la frecuencia a la que irá el PWM 4 kHz, los dos últimos ceros no se porqué, pero son necesarios ya que rcc_apb2_frequency también añade dos ceros a mayores
+  timer_set_repetition_counter(TIM8, 0);
+  timer_enable_preload(TIM8);
+  timer_continuous_mode(TIM8);
+  timer_set_period(TIM8, 1024);
+
+  timer_set_oc_mode(TIM8, TIM_OC1, TIM_OCM_PWM1);
+  timer_set_oc_mode(TIM8, TIM_OC2, TIM_OCM_PWM1);
+  timer_set_oc_mode(TIM8, TIM_OC3, TIM_OCM_PWM1);
+  timer_set_oc_mode(TIM8, TIM_OC4, TIM_OCM_PWM1);
+
+  timer_enable_break_main_output(TIM8);
+
+  timer_enable_counter(TIM8);
+}
+
 void setup() {
   setup_clock();
   setup_gpio();
   setup_usart();
   setup_timer_priorities();
   setup_leds_pwm();
+  setup_motors_pwm();
   setup_dma_adc1();
   setup_adc1();
   setup_systick();
