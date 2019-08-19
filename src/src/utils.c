82d281126038ded51e22c1e21732339c7710a0fe
Alex Santos <alex.santos.ete@gmail.com>
2019-08-19 16:42:38 +0200

    Pasa la función constrain a float

--- src/src/utils.c
+++ src/src/utils.c
@@ -4,7 +4,7 @@ float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-int32_t constrain(int32_t x, int32_t min, int32_t max) {
+float constrain(float x, float min, float max) {
   if (x > max) {
     return max;
   } else if (x < min) {
--- src/src/utils.h
+++ src/src/utils.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 float map(float x, float in_min, float in_max, float out_min, float out_max);
-int32_t constrain(int32_t x, int32_t min, int32_t max);
+float constrain(float x, float min, float max);
 
 #endif
